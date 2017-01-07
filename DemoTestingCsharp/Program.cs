8da2ac8af9e3a34e2b4017bc9814306498ab2c8b
Jin <Jin@JIN-ROG>
2017-01-07 16:18:36 +0200

    first program i made

---
+++ DemoTestingCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTestingCsharp
+{
+    class Program
+    {
+        // argumets
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("ТИ КОЙ СИ ? (Напиши си името)");
+            // set string to 'name' and later read args 
+            string name = Console.ReadLine();
+            // print string name 
+            Console.WriteLine("Здравей, " + name);
+            Console.WriteLine("ти спечели борбата с конзолката");
+        }
+    }
+}
+
---
+++ DemoTestingCsharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DemoTestingCsharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemoTestingCsharp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("804f04d6-3161-477d-98e4-ffd284b33ac1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
